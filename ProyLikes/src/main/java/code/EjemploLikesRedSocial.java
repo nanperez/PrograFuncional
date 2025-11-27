00a1434920bf775dc8ea79a732a6f94ac533bcef
nanperez <perez.castro.nancy@gmail.com>
2025-11-27 17:00:21 -0600

    Se actualizó Clase EjemploLikesRedSocial

--- ProyLikes/src/main/java/code/EjemploLikesRedSocial.java
+++ ProyLikes/src/main/java/code/EjemploLikesRedSocial.java
@@ -28,27 +28,40 @@ public class EjemploLikesRedSocial {
         // 2. Functions
 
         // a) Dar un like extra a cada publicación
-        
+        Function<Publicacion,Publicacion> darLike = p ->
+                p.conLikes(p.getLikes()+1);
 
         // b) Boost del algoritmo:
         //    si los likes son >= 100, se agregan 20 likes extra (post "tendencia")
-        
+        Function<Publicacion, Publicacion> boostAlgoritmo = p -> {
+            if (p.getLikes() >= 100) {
+                return p.conLikes(p.getLikes() + 20);
+            }
+            return p;
+        };
 
         // c) Calcular engagement en porcentaje (likes / vistas * 100)
         //    Se regresa un Double
-        
+        Function<Publicacion, Double> aEngagement =
+                p -> p.getVistas() == 0
+                        ? 0.0
+                        : (p.getLikes() * 100.0) / p.getVistas();
 
         // 3. Componer funciones:
         // primero dar like, luego aplicar boost, luego calcular engagement
-        
+        Function<Publicacion, Double> procesoEngagement =
+                darLike
+                        .andThen(boostAlgoritmo)
+                        .andThen(aEngagement);
+
 
         System.out.println("=== Engagement por publicación (después del algoritmo) ===");
-        // 4. Uso de Streams: aplicar el proceso a cada publicación
-        //feed.stream()
-                //.map(procesoEngagement)      // Stream<Double>
-                //.forEach(e ->
-                //        System.out.println(String.format("%.2f %%", e))
-                //);
+        //4. Uso de Streams: aplicar el proceso a cada publicación
+        feed.stream()
+                .map(procesoEngagement)      // Stream<Double>
+                .forEach(e ->
+                        System.out.println(String.format("%.2f %%", e))
+                );
 
         
     }
